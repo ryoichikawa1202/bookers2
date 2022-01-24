a3a02469b33c82bc91e65a39815338021cada764
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com>
2022-01-24 18:47:57 +0000

    [finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
 
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
-  validates :introduction, length: { maximum: 50 }, on: :update
+  validates :introduction, presence: false, length: {maximum: 50}
 
   def get_profile_image(size)
     unless profile_image.attached?
